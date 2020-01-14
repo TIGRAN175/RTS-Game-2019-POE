e0129c1fea0be94971e46b29ab8f90f30f2746da
tigranohannessian <your_email_address@example.com>
2020-01-14 19:45:16 +0200

    Final Commit of project including executable and source code

---
+++ Source Code/Assets/GameEngine.cs
@@ -0,0 +1,422 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using UnityEngine;
+namespace Assets
+{
+    class GameEngine
+    {
+        public Map map;
+        int remainingRounds;
+        int roundCount;     
+        int bonusResourcesTeam0;
+        int bonusResourcesTeam1; //Resources which are added when units die
+        GameManager gameManager;
+
+        public GameEngine(int numUnitsToCreate, int numBuildingsToCreate, int numRows, int numCols, int roundLimit, GameManager gameManager)
+        {
+            bonusResourcesTeam0 = 0;
+            bonusResourcesTeam1 = 0;
+            roundCount = 0;
+            remainingRounds = roundLimit;
+            map = new Map(numUnitsToCreate, numBuildingsToCreate, numRows, numCols);
+            map.GenerateMap(null);
+            this.gameManager = gameManager;
+
+
+        }
+
+        
+
+ 
+
+        public int PerformRound()
+        {
+            if (remainingRounds <= 0)
+            {
+                //done!!!
+                gameManager.gameOverMessage = "Round Limit Reached! It's a draw!";
+                gameManager.isGameOver = true;
+                return roundCount;
+            }
+
+            int totalReasourcesFromBuildingsTeam0 = 0;
+            int totalReasourcesFromBuildingsTeam1 = 0;
+
+
+                int buildingCountTeam0 = 0;
+                int buildingCountTeam1 = 0;
+            foreach (Building b in map.buildingList)
+            {
+
+                if (b is ResourceBuilding)
+                {
+                    ResourceBuilding r = (ResourceBuilding)b;
+
+
+                    //Debug.WriteLine("GENERATING RESOURCES");
+                    r.GenerateResourcesForRound();
+                    //now resourcesgenerated has been updated
+                    if (r.Team == 0)
+                    {
+                        buildingCountTeam0++;
+                        totalReasourcesFromBuildingsTeam0 += r.ResourcesGenerated;
+                    }
+                    else if (r.Team == 1)
+                    {
+                        buildingCountTeam1++;
+                        totalReasourcesFromBuildingsTeam1 += r.ResourcesGenerated;
+                    }
+
+
+                }
+            }
+
+            //int maxResourcesTeam1 = buildingCountTeam1 * 
+            int maxResourcesT0 = buildingCountTeam0 * ResourceBuilding.MAX_POOL_AMOUNT;
+            int maxResourcesT1 = buildingCountTeam1 * ResourceBuilding.MAX_POOL_AMOUNT;
+
+            foreach (Building b in map.buildingList)
+            {
+                if(b is ResourceBuilding)
+                {
+                    UnitDamageHandler h = b.UnityObject.GetComponent<UnitDamageHandler>() as UnitDamageHandler;
+                    if(b.Team == 0)
+                    {
+                        h.UpdateResources(totalReasourcesFromBuildingsTeam0, maxResourcesT0);
+                    }else if(b.Team ==1)
+                    {
+                        h.UpdateResources(totalReasourcesFromBuildingsTeam1, maxResourcesT1);
+                    }
+                }
+            }
+
+                
+            
+            gameManager.printToConsole("" + (totalReasourcesFromBuildingsTeam0 + bonusResourcesTeam0));
+            gameManager.printToConsole("" + (totalReasourcesFromBuildingsTeam1 + bonusResourcesTeam1));
+
+            //first the factories should spawn units then the round battle commenses 
+            for (int i =0; i < map.buildingList.Count; i++)
+            {
+                Building b = map.buildingList.ElementAt(i);
+                if (b is FactoryBuilding)
+                {
+                    FactoryBuilding f = (FactoryBuilding)b;
+                    int prodSpeed = f.GetProductionSpeed();
+                    if (roundCount % prodSpeed == 0)
+                    {
+                        if (b.Team == 0)
+                        {
+                            if (totalReasourcesFromBuildingsTeam0 - 5 >= 0)
+                            {
+                                //factory may produce a unit
+                                f.SpawnUnit(map);
+                                totalReasourcesFromBuildingsTeam0 = totalReasourcesFromBuildingsTeam0 - 5;
+                                gameManager.printToConsole("Building of team 0 spawned a unit" );
+
+                            }
+                            else if ((totalReasourcesFromBuildingsTeam0 + bonusResourcesTeam0 - 5) >= 0)
+                            {
+                                //we are able to spawn units because of the added bonus
+                                int toTakeFromBonus = 5 - totalReasourcesFromBuildingsTeam0;
+                                totalReasourcesFromBuildingsTeam0 = 0;
+                                bonusResourcesTeam0 -= toTakeFromBonus;
+                                f.SpawnUnit(map);
+                                gameManager.printToConsole("Building of team 0 spawned a unit");
+
+                            }
+                        }
+                        else if (b.Team == 1)
+                        {
+                            if (totalReasourcesFromBuildingsTeam1 - 5 >= 0)
+                            {
+                                //factory may produce a unit
+                                f.SpawnUnit(map);
+                                totalReasourcesFromBuildingsTeam1 -= 5;
+                                gameManager.printToConsole("Building of team 1 spawned a unit");
+
+                            }
+                            else if ((totalReasourcesFromBuildingsTeam1 + bonusResourcesTeam1 - 5) >= 0)
+                            {
+                                //we are able to spawn units because of the added bonus
+                                int toTakeFromBonus = 5 - totalReasourcesFromBuildingsTeam1;
+                                totalReasourcesFromBuildingsTeam1 = 0;
+                                bonusResourcesTeam1 -= toTakeFromBonus;
+                                f.SpawnUnit(map);
+                                gameManager.printToConsole("Building of team 1 spawned a unit");
+
+                            }
+                        }
+                    }
+
+                }
+
+                
+                gameManager.printToConsole(b.ToString());
+
+            }
+
+            foreach (Building b in map.buildingList)
+            {
+                if (b is ResourceBuilding)
+                {
+                    UnitDamageHandler h = b.UnityObject.GetComponent<UnitDamageHandler>() as UnitDamageHandler;
+                    if (b.Team == 0)
+                    {
+                        h.UpdateResources(totalReasourcesFromBuildingsTeam0, maxResourcesT0);
+                    }
+                    else if (b.Team == 1)
+                    {
+                        h.UpdateResources(totalReasourcesFromBuildingsTeam1, maxResourcesT1);
+                    }
+                }
+            }
+            gameManager.printToConsole("" + (totalReasourcesFromBuildingsTeam0 + bonusResourcesTeam0));
+            gameManager.printToConsole("" + (totalReasourcesFromBuildingsTeam1 + bonusResourcesTeam1));
+
+            List<Unit> unitList = map.GetUnitList();
+
+            for (int i = 0; i < unitList.Count; i++)
+            {
+                Unit u = unitList.ElementAt(i);
+
+                if (u is Wizard)
+                {
+                    //special wizard case
+                    if ((u.Health / (double)u.MaxHealth) * 100.0 <= 50)
+                    {
+                        map.MoveUnitRandomly(u);
+                        continue;
+                    }
+
+                    // perform special attack
+                    List<Unit> possibleUnitsToAttack = new List<Unit>();
+                    if (map.IsInMap(u.XPos - 1, u.YPos - 1))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos - 1, u.YPos - 1]); //topLeft
+                    }
+
+                    if (map.IsInMap(u.XPos - 1, u.YPos))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos - 1, u.YPos]);//toptop
+                    }
+
+                    if (map.IsInMap(u.XPos - 1, u.YPos + 1))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos - 1, u.YPos + 1]); //topRight
+                    }
+
+                    if (map.IsInMap(u.XPos, u.YPos + 1))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos, u.YPos + 1]); // rightRight
+                    }
+
+                    if (map.IsInMap(u.XPos + 1, u.YPos + 1))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos + 1, u.YPos + 1]);//bottomRight
+                    }
+
+                    if (map.IsInMap(u.XPos + 1, u.YPos))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos + 1, u.YPos]); // bottom bottom
+                    }
+
+                    if (map.IsInMap(u.XPos + 1, u.YPos - 1))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos + 1, u.YPos - 1]); // bottom left
+                    }
+
+                    if (map.IsInMap(u.XPos, u.YPos - 1))
+                    {
+                        possibleUnitsToAttack.Add(map.unitMap[u.XPos, u.YPos - 1]); // left left
+                    }
+
+                    //now check if there are targets at these positions
+                   
+                    bool hasAttacked = false;
+                    foreach (Unit victim in possibleUnitsToAttack)
+                    {
+                        if (victim != null)
+                        {
+                            if (victim.Team == 0 || victim.Team == 1)
+                            {
+                                hasAttacked = true;
+                                //blow things up
+                                bool unitIsNotDead = u.AttackUnit(victim, map);
+                                if (!unitIsNotDead)
+                                {
+                                    //unit died
+                                    if (victim.Team == 0)
+                                    {
+                                        bonusResourcesTeam0++;
+                                    }
+                                    else if (victim.Team == 1)
+                                    {
+                                        bonusResourcesTeam1++;
+                                    }
+
+                                    for(int j =0; j < unitList.Count; j++)
+                                    {
+                                        //scan for the victim
+                                        if(unitList.ElementAt(j) == victim)
+                                        {
+                                            unitList.RemoveAt(j);
+                                            if(j < i)
+                                            {
+                                                i--; // removing the victim from the list should not affect the iteration
+                                            }
+                                        }
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+
+                    if (!hasAttacked)
+                    {
+                        Position closestEnemyPos = u.FindClosestUnitOrBuilding(map); // in this case wizard can only find units
+                        if(closestEnemyPos.x == -1)
+                        {
+                            //wizards have won!!
+                            gameManager.gameOverMessage = "Wizards have won the game!";
+                            gameManager.isGameOver = true;
+                            // StopTimer();//////////////////
+                            return roundCount;
+                        }
+                        else
+                        {
+                            //move towards nearest enemy unit
+                            map.MoveTowardsEnemy(u, closestEnemyPos.x, closestEnemyPos.y);
+
+                        }
+                    }
+                    continue; // done with wizard
+                }
+                
+
+                //From here we know it is not a wizard
+                //Case 1: below health so run away
+                if ((u.Health / (double)u.MaxHealth) * 100.0 <= 25)
+                {
+                    map.MoveUnitRandomly(u);
+                    continue;
+                }
+
+                //Case 2: Finding enemy and decide on attacking
+
+                //Unit closestEnemy = u.FindClosestUnitOrBuilding(map);
+                Position closestEnemyPosition = u.FindClosestUnitOrBuilding(map);
+                if (closestEnemyPosition.x != -1)
+                {
+                    // the enemy can be a building or a unit
+                    bool isEnemyAUnit = false;
+                    if (map.unitMap[closestEnemyPosition.x, closestEnemyPosition.y] != null)
+                    {
+                        //we know a the enemy is a unit and not a building
+                        isEnemyAUnit = true;
+                    }
+
+                    if (isEnemyAUnit)
+                    {
+                        Unit closestEnemy = map.unitMap[closestEnemyPosition.x, closestEnemyPosition.y];
+                        if (map.IsWithinRange(u, closestEnemy))
+                        {
+                            // we can attack
+                            bool unitIsNotDead = u.AttackUnit(closestEnemy, map);
+                            if (!unitIsNotDead)
+                            {
+                                //unit died so add resources to its pool
+                                if (closestEnemy.Team == 0)
+                                {
+                                    bonusResourcesTeam0++;
+                                }
+                                else if (closestEnemy.Team == 1)
+                                {
+                                    bonusResourcesTeam1++;
+                                }
+
+                                for (int j = 0; j < unitList.Count; j++)
+                                {
+                                    //scan for the victim
+                                    if (unitList.ElementAt(j) == closestEnemy)
+                                    {
+                                        unitList.RemoveAt(j);
+                                        if (j < i)
+                                        {
+                                            i--; // removing the victim from the list should not affect the iteration
+                                        }
+                                    }
+                                }
+
+                            }
+                        }
+                        else
+                        {
+                            //we cant attack so move towards
+                            map.MoveTowardsEnemy(u, closestEnemyPosition.x, closestEnemyPosition.y);
+                        }
+                    }
+                    else
+                    {
+                        //There must be a building to attack at this position
+                        Building buildingToAttack = null;
+                        foreach (Building b in map.buildingList)
+                        {
+                            if (b.XPos == closestEnemyPosition.x && b.YPos == closestEnemyPosition.y)
+                            {
+                                buildingToAttack = b;
+                            }
+                        }
+
+                        if (map.IsWithinRange(u, buildingToAttack))
+                        {
+                            // we can attack the building
+                            bool buildingIsNotDead = u.AttackBuilding(buildingToAttack, map); // the deathhandler will deal with a building's death. No need to worry about it here
+                            if (!buildingIsNotDead)
+                            {
+                               
+                                gameManager.printToConsole("Building of team " + buildingToAttack.Team + " was destroyed!");
+
+                            }
+
+
+                        }
+                        else
+                        {
+                            //we cant attack so move towards the building
+                            map.MoveTowardsEnemy(u, closestEnemyPosition.x, closestEnemyPosition.y);
+                        }
+                    }
+                    //add units info to the killfeed
+                   
+                    gameManager.printToConsole(u.ToString());
+
+                }
+                else
+                {
+                    //Debug.WriteLine("Team " + u.Team + " WINS!!! -- no enemies left");
+                    // winLabel.Invoke(new Action(() => winLabel.Text = "Team " + u.Team + " has won the game!"));
+                    gameManager.printToConsole("Team " + (u.Team == 0 ? "Blue" : "Red") + " has won the game!");
+                    gameManager.gameOverMessage = "Team " + (u.Team == 0 ? "Blue" : "Red") + " has won the game!";
+                    gameManager.isGameOver = true;
+                    
+                    return roundCount;
+                }
+
+            }
+            //now round is done
+            roundCount++;
+            remainingRounds--;
+            return roundCount;
+        }
+
+       
+
+
+    }
+
+
+}
---
+++ Source Code/Assets/GameManager.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets
+{
+
+
+    public class GameManager : MonoBehaviour
+    {
+        public  GameObject BlueMeelee;
+        public  GameObject BlueRanged;
+        public  GameObject RedMeelee;
+        public  GameObject RedRanged;
+        public  GameObject GreenWizard;
+        public GameObject BlueResourceBuilding;
+        public GameObject RedResourceBuilding;
+        public GameObject BlueFactoryBuilding;
+        public GameObject RedFactoryBuilding;
+        public Text GameOverText;
+        public GameObject WinScreen;
+
+        float currentTime = 0f;
+        float startingTime = 10f;
+
+        bool toSpawn = true;
+        Map map;
+        GameEngine gameEngine;
+
+        public void DestroyGameObject(GameObject obj)
+        {
+            Destroy(obj);
+            
+        }
+
+        public bool isGameOver = false;
+        public string gameOverMessage = "";
+
+        public GameObject FactorySpawnUnit(Unit u)
+        {
+            var actualPos = new Vector3(u.XPos, 0, u.YPos);
+
+            if(u.Team ==0)
+            {
+                if(u is MeeleeUnit)
+                {
+
+                    return Instantiate(BlueMeelee, new Vector3(u.XPos, 1, u.YPos), Quaternion.Euler(new Vector3(0, 180, -90)));
+
+                }
+                else if (u is RangedUnit)
+                {
+                    return Instantiate(BlueRanged, actualPos, transform.rotation);
+
+                }
+            }
+            else if (u.Team == 1)
+            {
+                if (u is MeeleeUnit)
+                {
+                    return Instantiate(RedMeelee, new Vector3(u.XPos, 1, u.YPos), Quaternion.Euler(new Vector3(0, 180, -90)));
+
+                }
+                else if (u is RangedUnit)
+                {
+                    return Instantiate(RedRanged, actualPos, transform.rotation);
+
+                }
+
+            }
+            print("BIG ISSSUEEE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+            return null;
+        }
+
+        public void printToConsole(string message)
+        {
+            print(message);
+        }
+
+        // Use this for initialization
+         void Start()
+        {
+            currentTime = startingTime;
+            gameEngine = new GameEngine(20, 6, 20, 20, 40, this);
+            map = gameEngine.map;
+            //map = new Map(10, 2, 20, 20);
+
+
+            // meelee rotations: 0, 180, -90
+            //wiz  270, 0, 0
+            //GameObject unit = Instantiate(BlueMeelee, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 180, -90)));
+           /*
+            GameObject unit = Instantiate(RedMeelee, new Vector3(0, 0, 0), Quaternion.Euler(new Vector3(0, 180, -90)));
+
+            UnitDamageHandler h = unit.GetComponent<UnitDamageHandler>() as UnitDamageHandler;
+          // h = new UnitDamageHandler();
+           h.UpdateHealth(75, 100);
+           h.UpdateHealth(50, 100);
+            
+            */
+            //goal is to place units on the unity plane. including correct models and colors
+
+            //bind UNITS to GameObjects
+            
+            for (int i = 0; i < map.MAP_ROWS; i++)
+            {
+                for (int j = 0; j < map.MAP_COLS; j++)
+                {
+                    if (map.unitMap[i, j] != null)
+                    {
+                        //we have a wizard, ranged or meelee unit
+                        Unit unitToMap = map.unitMap[i, j];
+                        //unitToMap.UnityObject = new GameObject();
+                        var actualPos = new Vector3(unitToMap.XPos, 0, unitToMap.YPos);
+                        unitToMap.GameManager = this;
+
+                        if (unitToMap.Team == 0)
+                        {
+                            //blue team
+                            if (unitToMap is MeeleeUnit)
+                            {
+                                //unitToMap.UnityObject = Instantiate(BlueCube, actualPos, transform.rotation);
+                                unitToMap.UnityObject = Instantiate(BlueMeelee, new Vector3(unitToMap.XPos, 1, unitToMap.YPos), Quaternion.Euler(new Vector3(0, 180, -90)));
+                                
+                            }
+                            else if (unitToMap is RangedUnit)
+                            {
+                                unitToMap.UnityObject = Instantiate(BlueRanged, actualPos, transform.rotation);
+
+                            }
+                        }
+                        else if (unitToMap.Team == 1)
+                        {
+                            //Red team
+                            if (unitToMap is MeeleeUnit)
+                            {
+                                unitToMap.UnityObject = Instantiate(RedMeelee, new Vector3(unitToMap.XPos, 1, unitToMap.YPos), Quaternion.Euler(new Vector3(0, 180, -90)));
+
+                            }
+                            else if (unitToMap is RangedUnit)
+                            {
+                                unitToMap.UnityObject = Instantiate(RedRanged, actualPos, transform.rotation);
+
+                            }
+                        }
+                        else if (unitToMap.Team == 2)
+                        {
+                            //Wizards
+                            unitToMap.UnityObject = Instantiate(GreenWizard, actualPos, Quaternion.Euler(new Vector3(270, 0, 0)));
+
+                        }
+                    }
+                }
+            }
+            
+            //Place buildings on the unity plane
+            foreach (Building b in map.buildingList)
+            {
+                var actualPos = new Vector3(b.XPos, 0, b.YPos);
+                b.GameManager = this;
+                if (b.Team == 0)
+                {
+                    if(b is FactoryBuilding)
+                    {
+                        b.UnityObject = Instantiate(BlueFactoryBuilding, actualPos, Quaternion.Euler(new Vector3(270, 0, 0)));
+                    }
+                    else if(b is ResourceBuilding)
+                    {
+                        b.UnityObject = Instantiate(BlueResourceBuilding, actualPos, Quaternion.Euler(new Vector3(270, 0, 0)));
+
+                    }
+
+                }
+                else if (b.Team == 1)
+                {
+                    //grid.Rows[b.XPos].Cells[b.YPos].Style.BackColor = Color.Pink;
+                    if (b is FactoryBuilding)
+                    {
+                        b.UnityObject = Instantiate(RedFactoryBuilding, actualPos, Quaternion.Euler(new Vector3(270, 0, 0)));
+
+                    }
+                    else if (b is ResourceBuilding)
+                    {
+                        b.UnityObject = Instantiate(RedResourceBuilding, actualPos, Quaternion.Euler(new Vector3(270, 0, 0)));
+
+                    }
+                }
+            }
+            
+         
+        }
+
+        // Update is called once per frame
+        public float movementSpeed = 1;
+        GameObject obj1 = null, obj2 = null, obj3 = null;
+        int oldWholeNumber = -1;
+        void Update()
+        {
+           // isGameOver = true;
+            //gameOverMessage = "Round Limit Reached! It's a draw!";
+            if (isGameOver)
+            {
+                DisplayGameOver();
+                return;  
+            }
+           
+
+            currentTime = currentTime - 1 * Time.deltaTime;
+            string stringNumber = currentTime.ToString("0");
+            int wholeNumber = System.Int32.Parse(stringNumber);
+            if(oldWholeNumber != wholeNumber)
+            {
+                if ((wholeNumber) % 4 == 0)
+                {
+                    gameEngine.PerformRound();
+                }
+                oldWholeNumber = wholeNumber;
+            }
+
+            
+            
+
+        }
+
+        public void DisplayGameOver()
+        {
+            GameOverText.text = gameOverMessage;
+            //WinScreen.SetActive(true);
+        }
+
+        public void MoveUnitTowards( Vector3 toPos, GameObject unitToMove) 
+        {
+            StartCoroutine(MoveOverSeconds(unitToMove, toPos, 2f));
+        }
+
+        public GameObject CreateGameObject(GameObject prefab, Transform trans, Transform transRot)
+        {
+            // return new Instantiate(prefab, trans, transRot);
+            return null;
+        }
+
+
+        public IEnumerator MoveOverSeconds(GameObject objectToMove, Vector3 end, float seconds)
+        {
+            if(objectToMove == null)
+            {
+                print("Trying to Move corpses");
+                yield break;
+            }
+            float elapsedTime = 0;
+            Vector3 startingPos = objectToMove.transform.position;
+            while (elapsedTime < seconds)
+            {
+                objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+                elapsedTime += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            objectToMove.transform.position = end;
+        }
+
+
+
+    }
+}
